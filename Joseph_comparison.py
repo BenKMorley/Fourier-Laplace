3becc9ae9b81f5ce6c280c0115a86ea3104d9b67
BenKMorley <bkm1n18@soton.ac.uk>
2021-06-23 16:53:54 +0100

    Generalised to any dimension

--- Joseph_comparison.py
+++ Joseph_comparison.py
@@ -4,43 +4,32 @@ from numpy.core.numeric import zeros_like
 from numpy.fft import fftn, ifftn
 from tqdm import tqdm
 from multiprocessing import Pool
+import pickle
 
 ## General Parameters
 no_samples = 500
 L = 256
-alpha = 0
-beta = 0
-gamma = 0.1
-g = 0.1
-eps = 0.1  # The standard deviation of the Gaussian noise term
 q_fac = 2 * numpy.pi / L
 show_to = L // 4
 offsets = numpy.arange(0, 10)
+g = 0.1
 
 
 ## Define the expected q dependence function
-def analytic(q0, q1, q2, g=g, alpha=alpha, beta=beta, gamma=gamma):
-    q0_hat = 2 * numpy.sin(q0 / 2)
-    q1_hat = 2 * numpy.sin(q1 / 2)
-    q2_hat = 2 * numpy.sin(q2 / 2)
+def analytic(q_s, g, alpha, beta, gamma):
+    q_hat = []
+    L = q_s[0].shape[0]
+    dim = len(q_s)
+    q_sq = numpy.zeros((L, ) * dim)
 
-    q_sq = q0_hat ** 2 + q1_hat ** 2 + q2_hat ** 2
+    for d in range(dim):
+        q_hat = 2 * numpy.sin(q_s[d] / 2)
+        q_sq += q_hat ** 2
 
     return (q_sq / g ** 2) ** (3 / 2) * alpha \
         + beta * (q_sq / g ** 2) * (1 / 2) * \
         numpy.log(q_sq / g ** 2, out=numpy.zeros_like(q_sq), where=q_sq!=0) \
         + (gamma / g) * q_sq / g ** 2
-
-
-def my_fft(f_x):
-    x_s = numpy.arange(L)
-    q_s = numpy.arange(L) * numpy.pi * 2 / L
-
-    return numpy.sum(numpy.exp(-1j * numpy.outer(q_s, x_s)) * f_x, axis=1)
-
-
-def my_ifft(x_s):
-    return numpy.sum(numpy.exp(+1j * 2 * numpy.pi / L) * q_s)
 
 
 def Laplace_Transform_1D(data, offset=1):
@@ -60,73 +49,146 @@ def Laplace_Transform_1D(data, offset=1):
     return prefactor * numpy.sum(data * numpy.exp(-comb), axis=1)
 
 
+def Laplace_Transform_ND(data, dim, offset):
+    L = data.shape[0]
+    p_s = numpy.arange(L) * q_fac
+    assert len(offset) == dim
+
+    # prefactor = numpy.divide(1, 1 - numpy.exp(-p_s * L), out=numpy.zeros_like(p_s), where=p_s!=0)
+
+    result = numpy.zeros((L, ) * dim)
+
+    for d in range(dim):
+        x_s = (numpy.arange(L) + offset[d]) % L - offset[d]
+
+        comb = numpy.outer(p_s, x_s)
+        # factor = prefactor.reshape((L, 1)).repeat(L, axis=1) * numpy.exp(-comb)
+
+        data = numpy.tensordot(data, numpy.exp(-comb), axes=(d, 1))
+
+    return data
+
+
 q0_s = numpy.arange(L).reshape((L, 1, 1)).repeat(L, axis=1).repeat(L, axis=2) * q_fac
 q1_s = numpy.arange(L).reshape((1, L, 1)).repeat(L, axis=0).repeat(L, axis=2) * q_fac
 q2_s = numpy.arange(L).reshape((1, 1, L)).repeat(L, axis=1).repeat(L, axis=0) * q_fac
 
 
 def analysis(params):
+    ## Analysis
     alpha, beta, gamma, eps = params
 
-    data_q_3D = analytic(q0_s, q1_s, q2_s, alpha=alpha, beta=beta, gamma=gamma)
+    data_q_3D = analytic((q0_s, q1_s, q2_s), g, alpha, beta, gamma)
 
-    data_q_FT = numpy.zeros((no_samples, L))
-    data_q_LT = numpy.zeros((len(offsets), no_samples, L))
+    data_q_FT = numpy.zeros((no_samples, L, L, L))
+    data_q_LT = numpy.zeros((len(offsets), no_samples, L, L, L))
+    data_x_sum = numpy.zeros((len(offsets), no_samples, L), dtype=numpy.complex128)
 
-    for i in tqdm(range(no_samples)):
-        noise = eps * numpy.random.randn(L, L, L)
+    try:
+        data_full = pickle.load(open(f"data/fake_data/alpha{alpha}_beta{beta}_gamma{gamma}_eps{eps}_samples{no_samples}.pcl", "rb"))
+        data_q_FT, data_q_LT, data_x_sum = data_full
+        raise(Exception)
 
-        data_q = data_q_3D + noise
+    except Exception:
+        for i in tqdm(range(no_samples)):
+            noise = eps * numpy.random.randn(L, L, L)
 
-        data_x = fftn(data_q).real
+            data_q = data_q_3D + noise
 
-        data_x_1D = numpy.mean(data_x, axis=(1, 2))
+            data_x = fftn(data_q).real
 
-        data_q_FT[i] = fftn(data_x_1D).real
-        # data_q_FT2 = my_fft(data_x_1D)
+            data_x_1D = numpy.mean(data_x, axis=(1, 2))
 
-        for j, offset in enumerate(offsets):
-            data_q_LT[j, i] = Laplace_Transform_1D(data_x_1D, offset=offset)
+            data_q_FT[i] = fftn(data_x_1D).real
+
+            for j, offset in enumerate(offsets):
+                data_q_LT[j, i] = Laplace_Transform_1D(data_x_1D, offset=offset)
+
+                # Have a look in x-space of the IFT of FT + LT
+                data_x_sum[j, i] = ifftn(data_q_FT[i] + data_q_LT[j, i])
+
+        data_full = data_q_FT, data_q_LT, data_x_sum
+        pickle.dump(data_full, open(f"data/fake_data/alpha{alpha}_beta{beta}_gamma{gamma}_eps{eps}_samples{no_samples}.pcl", "wb"))
 
     q_s = numpy.arange(L) * numpy.pi * 2 / L
 
-    av = numpy.mean(data_q_FT, axis=0)[:show_to]
-    std = numpy.std(data_q_FT, axis=0)[:show_to]
-    plt.fill_between(q_s[:show_to], av - std, av + std)
-    plt.plot(q_s[:show_to], av)
-    plt.xlabel('q')
+    ## Plotting
+    av = numpy.mean(data_q_FT, axis=0)[1:show_to]
+    std = numpy.std(data_q_FT, axis=0)[1:show_to]
+    plt.fill_between(q_s[1:show_to] / g, av - std, av + std)
+    plt.plot(q_s[1:show_to] / g, av)
+    plt.xlabel('q / g')
     plt.title(f'Fourier Transform, alpha={alpha}, beta={beta}, gamma={gamma}, eps={eps}')
     plt.savefig(f'graphs/Fake_data_alpha{alpha}_beta{beta}_gamma{gamma}_eps{eps}_FT.png', dpi=500)
     plt.clf()
 
     for j, offset in enumerate(offsets):
-        av = numpy.mean(data_q_LT[j], axis=0)[:show_to]
-        std = numpy.std(data_q_LT[j], axis=0)[:show_to]
-        plt.fill_between(q_s[:show_to], av - std, av + std,
-                        label=f'offset={offset}')
-        plt.plot(q_s[:show_to], av)
-        plt.xlabel('q')
+        av = numpy.mean(data_q_LT[j], axis=0)[1:show_to]
+        std = numpy.std(data_q_LT[j], axis=0)[1:show_to]
+        plt.fill_between(q_s[1:show_to] / g, av - std, av + std,
+                         label=f'offset={offset}')
+        plt.plot(q_s[1:show_to] / g, av)
+        plt.xlabel('q / g')
         plt.title(f'Laplace Transform, offset={offset}, alpha={alpha}, beta={beta}, gamma={gamma}, eps={eps}')
         plt.savefig(f'graphs/Fake_data_alpha{alpha}_beta{beta}_gamma{gamma}_eps{eps}_LT_offset{offset}.png', dpi=500)
         plt.clf()
 
+        av = numpy.mean(data_x_sum[j], axis=0)[1:show_to]
+        std = numpy.std(data_x_sum[j], axis=0)[1:show_to]
+        plt.fill_between(q_s[1:show_to] / g, (av - std).real, (av + std).real,
+                         label='real', color='k')
+        plt.plot(q_s[1:show_to] / g, av.real, color='k')
+        plt.fill_between(q_s[1:show_to] / g, (av - std).imag, (av + std).imag,
+                         label='imag', color='r')
+        plt.plot(q_s[1:show_to] / g, av.imag, color='r')
+        plt.xlabel('x a')
+        plt.legend()
+        plt.title(f'Sum x-space, offset={offset}, alpha={alpha}, beta={beta}, gamma={gamma}, eps={eps}')
+        plt.savefig(f'graphs/Fake_data_alpha{alpha}_beta{beta}_gamma{gamma}_eps{eps}_x_sum_offset{offset}.png', dpi=500)
+        plt.clf()
+
     # Use my favourite offset of 1
     offset = 1
-    av = numpy.mean(data_q_FT + data_q_LT[numpy.argwhere(offsets == offset)[0, 0]], axis=0)[:show_to]
-    std = numpy.std(data_q_LT[numpy.argwhere(offsets == offset)[0, 0]] + data_q_FT, axis=0)[:show_to]
-    plt.fill_between(q_s[:show_to], av - std, av + std)
-    plt.plot(q_s[:show_to], av)
-    plt.xlabel('q')
+    av = numpy.mean(data_q_FT + data_q_LT[numpy.argwhere(offsets == offset)[0, 0]], axis=0)[1:show_to]
+    std = numpy.std(data_q_LT[numpy.argwhere(offsets == offset)[0, 0]] + data_q_FT, axis=0)[1:show_to]
+    plt.fill_between(q_s[1:show_to] / g, av - std, av + std)
+    plt.plot(q_s[1:show_to] / g, av)
+    plt.xlabel('q / g')
     plt.title(f'Fourier Transform + Laplace Transform, offset=1, alpha={alpha}, beta={beta}, gamma={gamma}, eps={eps}')
     plt.savefig(f'graphs/Fake_data_alpha{alpha}_beta{beta}_gamma{gamma}_eps{eps}_sum_offset{offset}.png', dpi=500)
     plt.clf()
 
 
+def analysis_ND_Laplace(params, dim=3):
+    alpha, beta, gamma, eps = params
+
+    q_s = numpy.arange(L) * numpy.pi * 2 / L
+
+    data_q_ND = analytic((q_s,) * dim, g, alpha, beta, gamma)
+
+    data_q_FT = numpy.zeros((no_samples, ) + (L, ) * dim)
+    data_q_LT = numpy.zeros((no_samples, ) + (L, ) * dim)
+    data_q_LT2 = numpy.zeros((no_samples, ) + (L, ) * dim)
+
+    for i in tqdm(range(no_samples)):
+        noise = eps * numpy.random.randn(*((L, ) * dim))
+
+        data_q = data_q_ND + noise
+
+        data_x = ifftn(data_q).real
+
+        data_q_FT[i] = fftn(data_x).real
+
+        data_q_LT[i] = Laplace_Transform_ND(data_x, dim=dim, offset=(1, ) * dim)
+        data_q_LT2[i] = Laplace_Transform_1D(data_x)
+
+        print("Hello")
+
+
 params = [[1, 0, 0, 0.1], [0, 1, 0, 0.1], [0, 0, 1, 0.1], [1, 1, 0.1, 0.1]]
 
 
+analysis_ND_Laplace(params[2], dim=1)
+
 p = Pool(4)
 p.map(analysis, params)
-
-# for param_set in params:
-#     analysis(param_set)
