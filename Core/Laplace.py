7cafc8afd7b52563fc229292c132c8c27759b6fb
BenKMorley <bkm1n18@soton.ac.uk>
2021-11-05 10:37:23 +0000

    Laplace Transforms are Core functionality in this project

---
+++ Core/Laplace.py
@@ -0,0 +1,45 @@
+import numpy
+
+
+def Laplace_Transform_ND(data, dim, offset, x_max=numpy.inf):
+    L = data.shape[0]
+    q_fac = 2 * numpy.pi / L
+    p_s = numpy.arange(L) * q_fac
+    assert len(offset) == dim
+
+    for d in range(dim):
+        x_s = (numpy.arange(L) + offset[d]) % L - offset[d]
+        comb = numpy.outer(p_s, x_s)
+        exp_comb = numpy.exp(-comb)
+
+        # We only need to use points at x = (-1, 0, 1). We therefore may choose to remove points
+        # to reduce the noise in the Laplace Transform
+        remove = x_s > x_max
+        exp_comb[:, remove] = 0
+
+        # Let D represent data and e represent the exponential comb. Use indices i, j, k to represent
+        # momentum space indices and x, y, z to represent position space indices
+        # D_ijk = D_zij e_k^z = (D_yzi e_j^y) e_k^z = ((D_xyz e_i^x) e_j^y) e_k^z
+        data = numpy.tensordot(data, exp_comb, axes=(0, 1))
+
+    return data
+
+
+def Laplace_Transform_1D(data, dim, offset, x_max=numpy.inf):
+    L = data.shape[0]
+    q_fac = 2 * numpy.pi / L
+    p_s = numpy.arange(L) * q_fac
+
+    result = numpy.zeros(L)
+
+    x_s = (numpy.arange(L) + offset) % L - offset
+    remove = x_s > x_max
+
+    for i, p in enumerate(p_s):
+        exp_comb = numpy.exp(-p * x_s)
+        exp_comb[remove] = 0
+
+        result[i] = numpy.sum(exp_comb * data)
+
+    return result
+
--- Local/analysis.py
+++ Local/analysis.py
@@ -15,52 +15,7 @@ sys.path.append(os.getcwd())
 sys.path.append(os.getcwd() + '/Core')
 
 from Core.MISC import GRID_convention_g, GRID_convention_L, GRID_convention_m, GRID_convention_N
-
-
-def Laplace_Transform_ND(data, dim, offset, x_max=numpy.inf):
-    L = data.shape[0]
-    q_fac = 2 * numpy.pi / L
-    p_s = numpy.arange(L) * q_fac
-    assert len(offset) == dim
-
-    for d in range(dim):
-        x_s = (numpy.arange(L) + offset[d]) % L - offset[d]
-        comb = numpy.outer(p_s, x_s)
-        exp_comb = numpy.exp(-comb)
-
-        # We only need to use points at x = (-1, 0, 1). We therefore may choose to remove points
-        # to reduce the noise in the Laplace Transform
-        remove = x_s > x_max
-        exp_comb[:, remove] = 0
-
-        # Let D represent data and e represent the exponential comb. Use indices i, j, k to represent
-        # momentum space indices and x, y, z to represent position space indices
-        # D_ijk = D_zij e_k^z = (D_yzi e_j^y) e_k^z = ((D_xyz e_i^x) e_j^y) e_k^z
-        data = numpy.tensordot(data, exp_comb, axes=(0, 1))
-
-    return data
-
-
-def Laplace_Transform_1D(data, dim, offset, x_max=numpy.inf):
-    L = data.shape[0]
-    q_fac = 2 * numpy.pi / L
-    p_s = numpy.arange(L) * q_fac
-
-    result = numpy.zeros(L)
-
-    # Project out the other dimensions
-    data = data[-1, -1] + data[-1, 0] + data[-1, 1] + data[0, -1] + data[0, 0] + data[0, 1] + data[1, -1] + data[1, 0] + data[1, 1]
-
-    x_s = (numpy.arange(L) + offset) % L - offset
-    remove = x_s > x_max
-
-    for i, p in enumerate(p_s):
-        exp_comb = numpy.exp(-p * x_s)
-        exp_comb[remove] = 0
-
-        result[i] = numpy.sum(exp_comb * data)
-
-    return result
+from Core.Laplace import Laplace_Transform_1D, Laplace_Transform_ND
 
 
 class analysis():
